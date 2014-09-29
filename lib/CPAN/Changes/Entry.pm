13d27ff8f06865f3f442e789448394b846199a21
Graham Knop <haarg@haarg.org>
2014-09-29 11:31:48 -0400

    repair entry serialization indenting

--- lib/CPAN/Changes/Entry.pm
+++ lib/CPAN/Changes/Entry.pm
@@ -12,16 +12,16 @@ sub _serialize {
   my $text = $self->text;
   if (length($style) < 2) {
     my $width = 80 - length($indent) - 2 * length($style);
-    $text =~ s/(^|\n)[ \t]*(.{1,$width}|\S+)(?=\s+|$)/$1$2\n/mg;
-    $text =~ s/\s+(?=\n)//g;
+    $text =~ s/\G[ \t]*([^\n]{1,$width}|\S+)(\s+|$)/$1$2\n/mg;
+    $text =~ s/[ \t]+\n/\n/g;
     my $space = ' ' x (2*length $style);
-    $text =~ s/^(.)/$1$space$2/mg;
-    $text = s/^ /$style/;
+    $text =~ s/^(.)/$space$1/mg;
+    $text =~ s/^ /$style/;
   }
   elsif (length $style == 2) {
     $text = substr($style, 0, 1) . " $text " . substr($style, 1, 1) . "\n";
   }
-  $text =~ s/^(.)/$indent$1/m;
+  $text =~ s/^(.)/$indent$1/mg;
   return $text;
 }
 
