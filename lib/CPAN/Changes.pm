ab77b159d7e4c3c8676ea1c3de9f98e62b41e241
Graham Knop <haarg@haarg.org>
2015-05-18 19:53:07 -0400

    Bumping version to 0.400001

--- lib/CPAN/Changes.pm
+++ lib/CPAN/Changes.pm
@@ -7,7 +7,7 @@ use CPAN::Changes::Release;
 use Scalar::Util ();
 use version      ();
 
-our $VERSION = '0.30';
+our $VERSION = '0.400001';
 
 # From DateTime::Format::W3CDTF
 our $W3CDTF_REGEX = qr{(\d\d\d\d) # Year
--- lib/Test/CPAN/Changes.pm
+++ lib/Test/CPAN/Changes.pm
@@ -7,7 +7,7 @@ use CPAN::Changes;
 use Test::Builder;
 use version ();
 
-our $VERSION = '0.30';
+our $VERSION = '0.400001';
 
 my $Test     = Test::Builder->new;
 
